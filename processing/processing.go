b973d2fd0a8cf0e803ed14f440c04ab00a162ff9
Laurène Tavella <59617674+LaureneT@users.noreply.github.com>
2023-09-16 16:19:29 -0400

    refactor processing functions

--- processing/processing.go
+++ processing/processing.go
@@ -6,7 +6,7 @@ import (
 )
 
 // ExtractProjectsFromReadme extracts GitHub repo URLs from README content.
-func ExtractProjectsFromReadme(readmeContent string) ([]map[string]string, error) {
+func ExtractProjectsFromReadme(readmeContent string) ([]Project, error) {
 	// Define a regular expression pattern to match GitHub repo URLs
 	re := regexp.MustCompile(`github\.com/([\w\-]+)/([\w\-]+)`)
 
@@ -14,7 +14,7 @@ func ExtractProjectsFromReadme(readmeContent string) ([]map[string]string, error
 	matches := re.FindAllStringSubmatch(readmeContent, -1)
 
 	// Extract the matched repo URLs
-	var projects []map[string]string
+	var projects []Project
 	for _, match := range matches {
 		if len(match) == 3 {
 			// The first element is the full match, the second and third elements are the owner and repo names
@@ -22,19 +22,34 @@ func ExtractProjectsFromReadme(readmeContent string) ([]map[string]string, error
 			repo := match[2]
 			// Construct the GitHub project URL
 			projectURL := "https://github.com/" + owner + "/" + repo
-			projectMap := map[string]string{"url": projectURL}
-			projects = append(projects, projectMap)
+			project := Project{Name: repo, URL: projectURL}
+			projects = append(projects, project)
 		}
 	}
 
 	return projects, nil
 }
 
+type Project struct {
+	Name string
+	URL  string
+}
 
-func FormatToJSON(projects []map[string]string) (string, error) {
-	// Create a map with a "projects" key
+func FormatToJSON(projects []Project) (string, error) {
+	// Create a slice to hold the URLs
+	var projectURLs []map[string]string
+
+	// Extract URLs from the Project structures
+	for _, project := range projects {
+		projectURL := map[string]string{
+			"url": project.URL,
+		}
+		projectURLs = append(projectURLs, projectURL)
+	}
+
+	// Create a map with a "projects" key and the project URLs
 	responseMap := map[string][]map[string]string{
-		"projects": projects,
+		"projects": projectURLs,
 	}
 
 	// Marshal the response map into JSON format
