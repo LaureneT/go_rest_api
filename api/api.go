d6116c15e943b4b7498ae7f2e6ae2e88c3d2bd01
Laurène Tavella <59617674+LaureneT@users.noreply.github.com>
2023-09-16 15:46:36 -0400

    finish separating network functions

--- api/api.go
+++ api/api.go
@@ -1,15 +1,10 @@
 package api
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"regexp"
-
-	"github.com/google/go-github/github"
-	"github.com/spf13/viper"
-	"golang.org/x/oauth2"
 
 	"github.com/LaureneT/go_rest_api/network"
 )
@@ -42,46 +37,6 @@ func HandleReadme(serverResponse http.ResponseWriter, clientRequest *http.Reques
 	fmt.Fprintln(serverResponse, readmeContent)
 }
 
-func GetREADME() (string, error) {
-	// Create a context
-	ctx := context.Background()
-
-	// Load the configuration file
-	viper.SetConfigFile("config.json")
-	if err := viper.ReadInConfig(); err != nil {
-		return "", fmt.Errorf("error reading config file: %v", err)
-	}
-
-	// Retrieve the GitHub access token from the configuration file
-	githubAccessToken := viper.GetString("github_access_token")
-	if githubAccessToken == "" {
-		return "", fmt.Errorf("GitHub access token is missing or empty in the configuration file")
-	}
-
-	// Create a GitHub client with your personal access token
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: githubAccessToken},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
-	owner := "avelino"
-	repo := "awesome-go"
-
-	// Fetch the README file from the repository
-	readme, _, err := client.Repositories.GetReadme(ctx, owner, repo, nil)
-	if err != nil {
-		return "", fmt.Errorf("error fetching README: %v", err)
-	}
-
-	// Decode the README content
-	readmeContent, err := readme.GetContent()
-	if err != nil {
-		return "", fmt.Errorf("error decoding README content: %v", err)
-	}
-
-	return readmeContent, nil
-}
-
 func HandleProjects(serverResponse http.ResponseWriter, clientRequest *http.Request) {
 	if clientRequest.URL.Path != "/projects" { // Necessary ?
 		http.NotFound(serverResponse, clientRequest)
@@ -89,7 +44,7 @@ func HandleProjects(serverResponse http.ResponseWriter, clientRequest *http.Requ
 	}
 
 	// Fetch the README content from GitHub
-	readmeContent, err := GetREADME()
+	readmeContent, err := network.FetchReadmeFromGitHub()
 	if err != nil {
 		http.Error(serverResponse, "Error fetching README", http.StatusInternalServerError)
 		fmt.Println("Error fetching README:", err)
